c948b71424c9c3cffce03876dcc64558fc85588a
silimelaL <lsilimela23@gmail.com>
2020-01-14 11:10:10 +0200

    adding soccer folders and making changies

---
+++ Soccer/index.php
@@ -0,0 +1,362 @@
+<?php
+	require_once 'functions/connect.php';
+?>
+<!doctype html>
+<!--[if lt IE 7]>		<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="zxx"> <![endif]-->
+<!--[if IE 7]>			<html class="no-js lt-ie9 lt-ie8" lang="zxx"> <![endif]-->
+<!--[if IE 8]>			<html class="no-js lt-ie9" lang="zxx"> <![endif]-->
+<!--[if gt IE 8]><!-->	<html class="no-js" lang="zxx"> <!--<![endif]-->
+
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Arnold Classic 2019</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="apple-touch-icon" href="apple-touch-icon.png">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/transitions.css">
+	<link rel="stylesheet" href="css/prettyPhoto.css">
+	<link rel="stylesheet" href="css/swiper.min.css">
+	<link rel="stylesheet" href="css/jquery-ui.css">
+	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="css/owl.theme.css">
+	<link rel="stylesheet" href="css/owl.carousel.css">
+	<link rel="stylesheet" href="css/customScrollbar.css">
+	<link rel="stylesheet" href="css/icomoon.css">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/color.css">
+	<link rel="stylesheet" href="css/responsive.css">
+	<script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+</head>
+<body>
+	<!--[if lt IE 8]>
+		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
+	<!--************************************
+			Wrapper Start
+	*************************************-->
+	<div id="tg-wrapper" class="tg-wrapper tg-haslayout">
+		<!--************************************
+				Mobile Menu Start
+		*************************************-->
+		<!--<div id="tg-navigationm-mobile" class="tg-navigationm-mobile tg-navigation collapse navbar-collapse">
+			<span id="tg-close" class="tg-close fa fa-close"></span>
+			<div class="tg-colhalf">
+				<ul>
+					<li class="active"> <!--- class="menu-item-has-children"></!--->
+						<!--a-- href="#">Main</!--a-->
+						<!--ul-- class="tg-dropdown-menu">
+							<li class="active"><a href="index-2.html">home1</a></li>
+							<li><a href="index2.html">home2</a></li>
+						</!--ul-->
+					<!--/li>
+					
+					
+				</ul>
+			</div>
+			<div class="tg-colhalf">
+				<ul>
+
+					<li><a href="contactus.html">Contact</a></li>
+					
+				</ul>
+			</div>
+		</div>-->
+		<!--************************************
+				Mobile Menu End
+		*************************************-->
+		<!--************************************
+				Header Start
+		*************************************-->
+		<header id="tg-header" class="tg-header tg-haslayout">
+			<div class="container">
+				<div class="col-md-10 col-md-offset-1 col-sm-12 col-xs-12">
+					<div class="row">
+						<div class="tg-topbar tg-haslayout">
+							<nav id="tg-topaddnav" class="tg-topaddnav">
+								<div class="navbar-header">
+									<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#tg-addnavigationm-mobile">
+										<i class="fa fa-arrow-right"></i>
+									</button>
+								</div>
+								<div id="tg-addnavigationm-mobile" class="tg-addnavigationm-mobile collapse navbar-collapse">
+									<div class="tg-colhalf pull-right">
+										<nav class="tg-addnav">
+											<ul>
+												<!--li--><!--a href="javascript().html" data-toggle="modal" data-target="#tg-login">login</!--a></!--li-->
+												
+												<li>
+													<a id="tg-btn-search" href="javascript:void(0)"><i class="fa fa-search"></i></a>
+												</li>
+											</ul>
+										</nav>
+									</div>
+									<div class="tg-colhalf">
+										<div class="tg-addnav text-center">
+											<ul>
+												<li><a href="javascript().html" data-toggle="modal" data-target="#tg-login">login</a></li>
+
+											</ul>
+										</div>
+									</div>
+								</div>
+							</nav>
+						</div>
+						<nav id="tg-nav" class="tg-nav brand-center">
+							<div class="navbar-header">
+								<!--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#tg-navigationm-mobile">
+									<i class="fa fa-bars"></i>
+								</button>-->
+								<strong class="tg-logo">
+									<a href="index-2.html"><img src="images/logo.png" alt="image description"></a>
+								</strong>
+							</div>
+							<div id="tg-navigation" class="tg-navigation">
+								<div class="tg-colhalf">
+
+                                </div>
+                                <div class="tg-colhalf">
+
+                                </div>
+							</div>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</header>
+		<!--************************************
+				Header End
+		*************************************-->
+		<!--************************************
+				Home Slider Start
+		*************************************-->
+		<div class="tg-sliderbox">
+			<div class="tg-imglayer">
+				<img src="images/bg-pattran.png" alt="image desctription">
+			</div>
+			<div id="tg-home-slider" class="tg-home-slider tg-haslayout">
+				<div class="swiper-wrapper">
+					<div class="swiper-slide">
+						<div class="container">
+							<figure class="floating">
+								<img src="images/slider/img-01.png" alt="image description">
+							</figure>
+							<div class="tg-slider-content">
+								<h1>arnold <span>classic 2019</span></h1>
+								<div class="tg-btnbox">
+									<h2>17 - 19 May</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="swiper-slide">
+						<div class="container">
+							<figure class="floating">
+								<img src="images/slider/img-01.png" alt="image description">
+							</figure>
+							<div class="tg-slider-content">
+								<h1>arnold <span>classic 2019</span></h1>
+								<div class="tg-btnbox">
+									<h2>17 - 19 May</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="swiper-slide">
+						<div class="container">
+							<figure class="floating">
+								<img src="images/slider/img-01.png" alt="image description">
+							</figure>
+							<div class="tg-slider-content">
+								<h1>arnold <span>classic 2019</span></h1>
+								<div class="tg-btnbox">
+									<h2>17 - 19 May</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="tg-btn-next">
+					<span>next</span>
+					<span class="fa fa-angle-down"></span>
+				</div>
+				<div class="tg-btn-prev">
+					<span>prev</span>
+					<span class="fa fa-angle-down"></span>
+				</div>
+			</div>
+		</div>
+		<!--************************************
+				Home Slider End
+		*************************************-->
+		<!--************************************
+				Main Start
+		*************************************-->
+		<main id="tg-main" class="tg-main tg-haslayout">
+
+			<!--************************************
+					Upcoming Match Start
+			*************************************-->
+			<section class=" tg-haslayout tg-bgstyleone">
+				<div class="tg-bgboxone"></div>
+				<div class="tg-bgboxtwo"></div>
+				<div class="tg-bgpattrant">
+					<div class="container">
+						<div class="row">
+							<div class="tg-upcomingmatch-counter">
+								<div class="col-md-4 col-sm-4 col-xs-12 hidden-xs">
+									<figure>
+										<img src="images/img-02.png" alt="image description">
+									</figure>
+								</div>
+								<div class="col-md-8 col-sm-8 col-xs-12 mt-2">
+									<div id="tg-upcomingmatch-slider" class="tg-upcomingmatch-slider tg-upcomingmatch">
+										<div class="swiper-wrapper">
+											<?php 
+											tournament();
+											?>
+										</div>
+										<div class="tg-themebtnnext"><span class="fa fa-angle-down"></span></div>
+										<div class="tg-themebtnprev"><span class="fa fa-angle-up"></span></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!--************************************
+					Upcoming Match End
+			*************************************-->
+
+			<!--************************************
+					Blog End
+			*************************************-->
+		</main>
+		<!--************************************
+				Main End
+		*************************************-->
+		<!--************************************
+				Footer Start
+		*************************************-->
+		<footer id="tg-footer" class="tg-footer tg-haslayout">
+			<!--<div class="tg-haslayout tg-footerinfobox">
+				<!--<div class="tg-bgboxone"></div>
+				<div class="tg-bgboxtwo"></div>-->
+				<!--<div class="tg-footerinfo">
+					<div class="container">
+						<div class="row">
+						</div>
+					</div>
+				</div>-->
+			<!--/div>-->
+			<div class="tg-footerbar">
+				<div class="container">
+					<span class="tg-copyright"><a target="_blank" href="#">Templates Hub</a></span>
+					<nav class="tg-footernav">
+						<ul>
+							<li><a href="#">Main</a></li>
+							<li><a href="#">Team</a></li>
+							<li><a href="#">Buy Tickets</a></li>
+							<li><a href="#">Match Results</a></li>
+							<li><a href="#">Upcoming Matches</a></li>
+							<li><a href="#">Shop</a></li>
+							<li><a href="#">Contact</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</footer>
+		<!--************************************
+				Footer End
+		*************************************-->
+	</div>
+	<!--************************************
+			Wrapper End
+	*************************************-->
+	<!--************************************
+			Search Start
+	*************************************-->
+	<div class="tg-searchbox">
+		<a id="tg-close-search" class="tg-close-search" href="javascript:void(0)"><span class="fa fa-close"></span></a>
+		<div class="tg-searcharea">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6 col-sm-offset-3">
+						<form class="tg-form-search">
+							<fieldset>
+								<input type="search" class="form-control" placeholder="keyword">
+								<i class="icon-icon_search2"></i>
+							</fieldset>
+						</form>
+						<p>Start typing and press Enter to search</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--************************************
+			Search End
+	*************************************-->
+	<!--************************************
+		LightBoxes Start
+	*************************************-->
+	<div class="tg-modalbox modal fade" id="tg-login" tabindex="-1" role="dialog">
+		<div class="modal-dialog" role="document">
+			<div class="tg-modal-content">
+				<div class="tg-formarea text-center justify-content-center">
+					<div class="tg-border-heading">
+						<h3>Login</h3>
+					</div>
+					<form class="tg-loginform" method="post">
+						<fieldset>
+							<div class="form-group">
+								<input type="text" name="userName/email" class="form-control" placeholder="username/email">
+							</div>
+							<div class="form-group">
+								<input type="password" name="password" class="form-control" placeholder="password">
+							</div>
+							<div class="form-group">
+								<label>
+									<input type="checkbox" value="rememberme" class="checkbox">
+									<em>Remember Me</em>
+								</label>
+								<a href="#">
+									<em>Forgot Password</em>
+									<i class="fa fa-question-circle"></i>
+								</a>
+							</div>
+							<div class="form-group">
+								<button class="tg-btn tg-btn-lg" type="submit">Login Now</button>
+							</div>
+							
+						</fieldset>
+					</form>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!--************************************
+		LightBoxes End
+	*************************************-->
+	<script src="js/vendor/jquery-library.js"></script>
+	<script src="js/vendor/bootstrap.min.js"></script>
+	<script src="js/customScrollbar.min.js"></script>
+	<script src="js/owl.carousel.js"></script>
+	<script src="js/isotope.pkgd.js"></script>
+	<script src="js/prettyPhoto.js"></script>
+	<script src="js/swiper.min.js"></script>
+	<script src="js/jquery-ui.js"></script>
+	<script src="js/countTo.js"></script>
+	<script src="js/appear.js"></script>
+	<script src="js/main.js"></script>
+</body>
+
+
+</html>
